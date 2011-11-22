fdbb247c8084e5ef5b2d946f196ae34e06481134
Rahul Gathoo <rahul.gathoo@cmcltd.com>
2011-11-22 12:01:43 +0530

    My 1st LocalRepo commit

---
+++ MyRemoteProject/src/com/HelloRemote.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloRemote {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
